3dc0caa2fa1741d927cfbd7e7e9939adfebcb6e8
aashishthapaliya97-hub <Aashishthapaliya97@gmail.com>
2025-12-16 08:50:03 +0545

    latest change

---
+++ Titanic_data.py
@@ -0,0 +1,40 @@
+import seaborn as sns
+import matplotlib.pyplot as plt 
+import pandas as pd 
+import numpy as np  
+import streamlit as st  
+
+df = sns.load_dataset('titanic')
+
+st.title("Titanic Dataset Analysis")
+st.write("This application analyzes the Titanic dataset using various visualizations.")
+
+
+df_clean = df.dropna(subset=['age', 'embarked'])
+st.header('Survival Count by Passenger Class')
+fig1, ax1 = plt.subplots()
+sns.countplot(data=df_clean, x='class', hue='survived')
+st.pyplot(fig1)
+
+st.header('Age Distribution: Survivors vs Non-Survivors')
+fig2, ax2 = plt.subplots()
+sns.histplot(data=df_clean, x='age', hue='survived', bins=30, kde=True)
+st.pyplot(fig2)
+
+survival_by_gender = df_clean.groupby('sex')['survived'].mean() * 100
+
+st.header('Survival Rate (%)')
+fig3, ax3 = plt.subplots()
+sns.barplot(x=survival_by_gender.index, y=survival_by_gender.values)
+st.pyplot(fig3)
+
+
+
+avg_fare_by_class = df_clean.groupby('class')['fare'].mean()
+
+
+st.header('Average Fare ($)')
+
+fig4, ax4 = plt.subplots()
+avg_fare_by_class.plot(kind='bar', color=['#F18F01', '#C73E1D', '#6A994E'])
+st.pyplot(fig4)
